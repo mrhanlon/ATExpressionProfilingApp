e255fc9f1ee0692fbe232bb94f42dce0e59dbc5b
eriksf <erik@jcvi.org>
2014-10-27 13:31:27 -0400

    switched to using bower-provided datatables-bootstrap3 lib

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,7 +14,7 @@
     appContext.html('<h2 id="toggleHelp">Hello AIP Science App &plus; Agave API!</h2><div class="api-help list-group"></div><hr><div class="api-info"></div><br>');
 
     help = $('.api-help', appContext);
-    
+
     $('#toggleHelp').click(function() {
       $('.api-help').toggle();
     });
@@ -57,29 +57,29 @@
     info.addClass('text-center');
     info.append('<p>' + Agave.api.info.title + ': ' + Agave.api.info.description + '</p>');
     info.append('<p><a href="mailto:' + Agave.api.info.contact + '">Contact</a> | <a href="' + Agave.api.info.license + '">License</a> | <a href="' + Agave.api.info.license + '">Terms of use</a></p>');
-    
+
     var DEBUG, log, init, renderExpressionTable;
-    
+
       DEBUG = true;
       log = function log( message ) {
         if ( DEBUG ) {
           console.log( message );
         }
       };
-    
+
       init = function init() {
           log( 'Initializing app...' );
       };
-    
+
       //draw interactions table
       renderExpressionTable = function renderExpressionTable(url, gene, tissue) {
         $('#expression_itable').empty();
-    
+
         var myUrl = url;
         if (url.substr(-1) === '/') {
           myUrl = url.replace(/\/$/, '');
         }
-    
+
         if (tissue !== 'none') {
             myUrl += '?transcript=' + gene + '&material=' + tissue;
         } else {
@@ -87,7 +87,7 @@
         }
         log('Grabbing data from ' + myUrl);
         log('Using authorization header: "Authorization: Bearer ' + Agave.token.accessToken + '"');
-        
+
         $('#expression_itable').html('<table cellspacing="0" class="table table-striped table-bordered" id="etable"></table>');
         $('#etable').prepend('<caption>Results from the Arabidopsis 2010 Expression Database</caption>');
         $('#etable').dataTable( {
@@ -113,17 +113,17 @@
                 { "data": "expression_record.0.absolute_concentration_stdev", "title": "Std dev (+)" },
             ]
         } );
-    
+
         $('#expression_itable').removeClass('hidden');
       };
-    
+
       /* go! */
       if (! $('#expression_viewer').hasClass('expression-viewer-processed') ) {
         // prevent duplicate initialization
         $('#expression_viewer').addClass('expression-viewer-processed');
-    
+
         init();
-    
+
         $('#expression_gene_form_reset').on('click', function() {
           $('#expression_itable').empty();
           $('#expression_itable').addClass('hidden');
@@ -131,12 +131,12 @@
           $('#expression_tissue').val('none');
           $('.result').empty();
         });
-    
+
         $( 'form[name=expression_gene_form]' ).on( 'submit', function( e ) {
           e.preventDefault();
-    
+
           var url = 'https://api.araport.org/community/v0.3/erik-dev/expression_per_gene_tissue_02_v0.2/search';
-    
+
           $('.result').empty();
           var gene = $('#expression_gene').val();
           var tissue = $('#expression_tissue').val();
@@ -144,10 +144,10 @@
           if (gene.length > 0) {
               renderExpressionTable(url, gene, tissue);
           } else {
-            window.alert('You must enter a gene first.');
+            window.alert('You must enter a gene first!');
           }
         }); /// end gene submit function
       }
   });
-  
+
 })(window, jQuery);
