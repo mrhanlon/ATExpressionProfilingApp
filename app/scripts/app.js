c72f8a105bf900afe5924ee5f2e9b3b438758b4f
eriksf <erik@jcvi.org>
2015-06-20 19:25:02 -0400

    first crack at reporter images

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,7 +24,22 @@
     var templates = {
         resultTable: _.template('<table class="table table-striped table-bordered"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material</th><th>Cycle Time</th><th>Std dev (+)</th><th>Ratio to Invariants</th><th>Std dev (+)</th><th>Absolute Concentration</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-ellipsis-h"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_record.material_text_description %></td><td><%= r.expression_record.cycle_time %></td><td><%= r.expression_record.cycle_time_stdev %></td><td><%= r.expression_record.ratio_to_invariants %></td><td><%= r.expression_record.ratio_to_invariants_stdev %></td><td><%= r.expression_record.absolute_concentration %></td><td><%= r.expression_record.absolute_concentration_stdev %></td></tr><% }) %></tbody></table>'),
         comparisonTable: _.template('<table class="table table-striped table-bordered"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material 1</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th><th>Material 2</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-ellipsis-h"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_comparison_record.material1_text_description %></td><td><%= r.expression_comparison_record.expression_value_material1 %></td><td><%= r.expression_comparison_record.expression_value_material1_stdev %></td><td><%= r.expression_comparison_record.material2_text_description %></td><td><%= r.expression_comparison_record.expression_value_material2 %></td><td><%= r.expression_comparison_record.expression_value_material2_stdev %></td></tr><% }) %></tbody></table>'),
-        geneReport: _.template('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" data-dismiss="modal" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4>Gene Report: <%= locus %></h4></div><div class="modal-body"><% _.each(properties, function(prop) { %><h3><%= prop.type.replace("_"," ") %></h3><p><%= prop.value %></p><% }) %></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>')
+        geneReport: _.template('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" data-dismiss="modal" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4>Gene Report: <%= locus %></h4></div><div class="modal-body"><% _.each(properties, function(prop) { %><h3><%= prop.type.replace("_"," ") %></h3><p><%= prop.value %></p><% }) %></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>'),
+        imageTable: _.template('<table class="table table-striped table-bordered">' +
+                                  '<thead><tr>' +
+                                  '<th></th>' +
+                                  '<th>Locus</th>' +
+                                  '<th>Line</th>' +
+                                  '</tr></thead><tbody>' +
+                                  '<% _.each(result, function(r) { %>' +
+                                  '<tr>' +
+                                  '<td>+</td>' +
+                                  '<td><%= r.locus %></td>' +
+                                  '<td><%= r.line_record.line_id %></td>' +
+                                  '</tr>' +
+                                  '<% }) %>' +
+                                  '</tbody>' +
+                                  '</table>')
     };
 
     //draw individual gen expression table
@@ -89,6 +104,18 @@
         $('.comp_results table', appContext).dataTable( {'lengthMenu': [5, 10, 25, 50, 100]} );
     };
 
+    // draw reporter image table
+    var showImageResults = function showImageResults(json) {
+
+        if ( ! (json && json.obj) || json.obj.status !== 'success') {
+            $('.reporter_image_results', appContext).html('<div class="alert alert-danger">Invalid response!</div>');
+            return;
+        }
+
+        $('.reporter_image_results', appContext).html(templates.imageTable(json.obj));
+        $('.reporter_image_results table', appContext).dataTable( {'lengthMenu': [5, 10, 25, 50, 100]} );
+    };
+
     var showError = function(err) {
         $('.error', appContext).html('<div class="alert alert-danger">Error contacting the server! Please try again later.</div>');
         console.error('Status: ' + err.obj.status + '  Message: ' + err.obj.message);
@@ -111,6 +138,28 @@
         $('#expression_tissue1').val('');
         $('#expression_tissue2').val('');
     });
+
+    $('#reporter_image_form_reset').on('click', function() {
+        $('.error').empty();
+        $('.reporter_image_results').empty();
+        $('#ri_gene').val('');
+    });
+
+    $('form[name=reporter_image_form]').on('submit', function(e) {
+        e.preventDefault();
+
+        var query = {
+            locus: this.ri_gene.value,
+        };
+
+        $('.reporter_image_results').empty();
+        $('.error').empty();
+        Agave.api.adama.search({
+            'namespace': 'eriksf-dev',
+            'service': 'lines_by_locus_v0.1',
+            'queryParams': query
+        }, showImageResults, showError);
+    }); /// end gene submit function
 
     $('form[name=expression_gene_form]').on('submit', function(e) {
         e.preventDefault();
