ca1b888f3fcb773531b3e82743a1ad2a19ba2ed2
eriksf <erik@jcvi.org>
2015-06-29 18:59:04 -0400

    changed "info" button reports from modal dialogs to popovers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,10 +24,9 @@
     var imageSrc = 'http://www.jcvi.org/arabidopsis/qpcr/get_image.php';
 
     var templates = {
-        resultTable: _.template('<table class="table table-striped table-bordered"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material</th><th>Cycle Time</th><th>Std dev (+)</th><th>Ratio to Invariants</th><th>Std dev (+)</th><th>Absolute Concentration</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_record.material_text_description %></td><td><%= r.expression_record.cycle_time %></td><td><%= r.expression_record.cycle_time_stdev %></td><td><%= r.expression_record.ratio_to_invariants %></td><td><%= r.expression_record.ratio_to_invariants_stdev %></td><td><%= r.expression_record.absolute_concentration %></td><td><%= r.expression_record.absolute_concentration_stdev %></td></tr><% }) %></tbody></table>'),
-        comparisonTable: _.template('<table class="table table-striped table-bordered"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material 1</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th><th>Material 2</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_comparison_record.material1_text_description %></td><td><%= r.expression_comparison_record.expression_value_material1 %></td><td><%= r.expression_comparison_record.expression_value_material1_stdev %></td><td><%= r.expression_comparison_record.material2_text_description %></td><td><%= r.expression_comparison_record.expression_value_material2 %></td><td><%= r.expression_comparison_record.expression_value_material2_stdev %></td></tr><% }) %></tbody></table>'),
-        geneReport: _.template('<div class="modal fade"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" data-dismiss="modal" class="close"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button><h4>Gene Report: <%= locus %></h4></div><div class="modal-body"><% _.each(properties, function(prop) { %><h3><%= prop.type.replace("_"," ") %></h3><p><%= prop.value %></p><% }) %></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>'),
-        imageTable: _.template('<table class="table table-striped table-bordered image-table">' +
+        resultTable: _.template('<table class="table table-striped table-bordered at-table"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material</th><th>Cycle Time</th><th>Std dev (+)</th><th>Ratio to Invariants</th><th>Std dev (+)</th><th>Absolute Concentration</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-visible="false" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_record.material_text_description %></td><td><%= r.expression_record.cycle_time %></td><td><%= r.expression_record.cycle_time_stdev %></td><td><%= r.expression_record.ratio_to_invariants %></td><td><%= r.expression_record.ratio_to_invariants_stdev %></td><td><%= r.expression_record.absolute_concentration %></td><td><%= r.expression_record.absolute_concentration_stdev %></td></tr><% }) %></tbody></table>'),
+        comparisonTable: _.template('<table class="table table-striped table-bordered at-table"><caption>Results from the Arabidopsis 2010 Expression Database</caption><thead><tr><th>Gene</th><th>Material 1</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th><th>Material 2</th><th>Expression Value (fmol/mg)</th><th>Std dev (+)</th></tr></thead><tbody><% _.each(result, function(r) { %><tr><td><%= r.transcript %> <button name="gene-report" data-visible="false" data-locus="<%= r.transcript %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle"></i><span class="sr-only">Get Gene Report</span></button></td><td><%= r.expression_comparison_record.material1_text_description %></td><td><%= r.expression_comparison_record.expression_value_material1 %></td><td><%= r.expression_comparison_record.expression_value_material1_stdev %></td><td><%= r.expression_comparison_record.material2_text_description %></td><td><%= r.expression_comparison_record.expression_value_material2 %></td><td><%= r.expression_comparison_record.expression_value_material2_stdev %></td></tr><% }) %></tbody></table>'),
+        imageTable: _.template('<table class="table table-striped table-bordered at-table image-table">' +
                                   '<thead><tr>' +
                                   '<th></th>' +
                                   '<th>Gene</th>' +
@@ -36,13 +35,13 @@
                                   '<% _.each(result, function(r) { %>' +
                                   '<tr>' +
                                   '<td class="details-control"><i class="fa fa-plus-square fa-lg"></i></td>' +
-                                  '<td><%= r.locus %><button name="gene-report" data-locus="<%= r.locus %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle fa-lg"></i><span class="sr-only">Get Gene Report</span></button></td>' +
+                                  '<td><%= r.locus %><button name="gene-report" data-visible="false" data-locus="<%= r.locus %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle fa-lg"></i><span class="sr-only">Get Gene Report</span></button></td>' +
                                   '<td><%= r.line_record.line_id %></td>' +
                                   '</tr>' +
                                   '<% }) %>' +
                                   '</tbody>' +
                                   '</table>'),
-        imageDetailRow: _.template('<table class="table table-striped table-bordered image-detail-table">' +
+        imageDetailRow: _.template('<table class="table table-striped table-bordered at-table image-detail-table">' +
                                    '<thead><tr>' +
                                    '<th></th>' +
                                    '<th>PO Code</th>' +
@@ -59,7 +58,7 @@
                                    '<% if (r.image_record.po_codes.length > 1) { %>' +
                                    '<% for(i=1; i < r.image_record.po_codes.length; i++) { %>' +
                                    '<tr>' +
-                                   '<td><%= r.image_record.po_codes[i].po_code %><button name="po-report" data-po_code="<%= r.image_record.po_codes[i].po_code %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle fa-lg"></i><span class="sr-only">Get PO Report</span></button></td>' +
+                                   '<td><%= r.image_record.po_codes[i].po_code %><button name="po-report" data-visible="false" data-po_code="<%= r.image_record.po_codes[i].po_code %>" class="btn btn-link btn-sm"><i class="fa fa-info-circle fa-lg"></i><span class="sr-only">Get PO Report</span></button></td>' +
                                    '<td><%= r.image_record.po_codes[i].po_name %></td>' +
                                    '<td><%= r.image_record.po_codes[i].expression %></td>' +
                                    '</tr>' +
@@ -67,24 +66,10 @@
                                    '<% } %>' +
                                    '<% }) %>' +
                                    '</tbody></table>'),
-        poReport: _.template('<div class="modal fade">' +
-                             '<div class="modal-dialog">' +
-                             '<div class="modal-content">' +
-                             '<div class="modal-header">' +
-                             '<button type="button" data-dismiss="modal" class="close">' +
-                             '<span aria-hidden="true">&times;</span><span class="sr-only">Close</span>' +
-                             '</button>' +
-                             '<h4>Plant Ontology Report - <%= po_code %></h4>' +
-                             '</div>' +
-                             '<div class="modal-body">' +
-                             '<h3>Accession</h3><p><%= po_code %></p>' +
+        poReportPopover: _.template('<h3>Accession</h3><p><%= po_code %></p>' +
                              '<h3>Name</h3><p><%= po_definition_record.po_name %></p>' +
                              '<h3>Namespace</h3><p><%= po_definition_record.po_namespace %></p>' +
-                             '<h3>Definition</h3><p><%= po_definition_record.po_def %></p>' +
-                             '</div>' +
-                             '<div class="modal-footer">' +
-                             '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
-                             '</div></div></div></div>'),
+                             '<h3>Definition</h3><p><%= po_definition_record.po_def %></p>'),
         imageFull: _.template('<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">' +
                               '<div class="modal-dialog">' +
                               '<div class="modal-content">' +
@@ -99,6 +84,40 @@
                               '<div class="modal-footer">' +
                               '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
                               '</div></div></div></div>'),
+        geneReportPopover: _.template('<% _.each(properties, function(prop) { %>' +
+                                '<h3><%= prop.type.replace("_"," ") %></h3>' +
+                                '<p><%= prop.value %></p>' +
+                                '<% }) %>'),
+    };
+
+    // gene report handler
+    var geneReportHandler = function geneReportHandler() {
+        $('button[name=gene-report]', appContext).on('click', function(e) {
+            e.preventDefault();
+            var el = $(this);
+            if (el.data('visible')) {
+                el.popover('hide');
+                el.data('visible', false);
+            } else {
+                var locus = el.attr('data-locus');
+                if (locus.indexOf('.') !== -1) {
+                    locus = locus.slice(0, locus.indexOf('.'));
+                }
+                var query = { locus: locus };
+                Agave.api.adama.search(
+                    {'namespace': 'aip', 'service': 'locus_gene_report_v0.2.0', 'queryParams': query},
+                    function(search) {
+                        el.popover({title: 'Gene Report: ' + locus,
+                                    content: templates.geneReportPopover(search.obj.result[0]),
+                                    trigger: 'manual',
+                                    html: true,
+                                    template: '<div class="popover popover-definition" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'});
+                        el.popover('show');
+                        el.data('visible', true);
+                    }
+                );
+            }
+        });
     };
 
     //draw individual gen expression table
@@ -111,23 +130,7 @@
 
         $('.gene_results', appContext).html(templates.resultTable(json.obj));
 
-        $('button[name=gene-report]', appContext).on('click', function(e) {
-            e.preventDefault();
-
-            var locus = $(this).attr('data-locus');
-
-            var query = {
-                locus: locus.slice(0, locus.indexOf('.'))
-            };
-
-            Agave.api.adama.search(
-                {'namespace': 'aip', 'service': 'locus_gene_report_v0.2.0', 'queryParams': query},
-                function(search) {
-                    var html = templates.geneReport(search.obj.result[0]);
-                    $(html).appendTo('body').modal();
-                }
-            );
-        });
+        geneReportHandler();
 
         $('.gene_results table', appContext).dataTable( {'lengthMenu': [5, 10, 25, 50, 100]} );
     };
@@ -142,23 +145,7 @@
 
         $('.comp_results', appContext).html(templates.comparisonTable(json.obj));
 
-        $('button[name=gene-report]', appContext).on('click', function(e) {
-            e.preventDefault();
-
-            var locus = $(this).attr('data-locus');
-
-            var query = {
-                locus: locus.slice(0, locus.indexOf('.'))
-            };
-
-            Agave.api.adama.search(
-                {'namespace': 'aip', 'service': 'locus_gene_report_v0.2.0', 'queryParams': query},
-                function(search) {
-                    var html = templates.geneReport(search.obj.result[0]);
-                    $(html).appendTo('body').modal();
-                }
-            );
-        });
+        geneReportHandler();
 
         $('.comp_results table', appContext).dataTable( {'lengthMenu': [5, 10, 25, 50, 100]} );
     };
@@ -206,40 +193,30 @@
             }
         } );
 
-        $('button[name=gene-report]', appContext).on('click', function(e) {
-            e.preventDefault();
-
-            var locus = $(this).attr('data-locus');
-
-            var query = {
-                locus: locus
-            };
-
-            Agave.api.adama.search(
-                {'namespace': 'aip', 'service': 'locus_gene_report_v0.2.0', 'queryParams': query},
-                function(search) {
-                    var html = templates.geneReport(search.obj.result[0]);
-                    $(html).appendTo('body').modal();
-                }
-            );
-        });
+        geneReportHandler();
 
         $('.reporter_image_results table', appContext).on('click', 'button[name=po-report]', function(e) {
             e.preventDefault();
-
-            var po_code = $(this).attr('data-po_code');
-
-            var query = {
-                po_code: po_code
-            };
-
-            Agave.api.adama.search(
-                {'namespace': 'jcvi', 'service': 'podefinition_by_code_v0.1', 'queryParams': query},
-                function(search) {
-                    var html = templates.poReport(search.obj.result[0]);
-                    $(html).appendTo('body').modal();
-                }
-            );
+            var el = $(this);
+            if (el.data('visible')) {
+                el.popover('hide');
+                el.data('visible', false);
+            } else {
+                var po_code = $(this).attr('data-po_code');
+                var query = { po_code: po_code };
+                Agave.api.adama.search(
+                    {'namespace': 'jcvi', 'service': 'podefinition_by_code_v0.1', 'queryParams': query},
+                    function(search) {
+                        el.popover({title: 'Plant Ontology Report - ' + po_code,
+                                    content: templates.poReportPopover(search.obj.result[0]),
+                                    trigger: 'manual',
+                                    html: true,
+                                    template: '<div class="popover popover-definition" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="popover-content"></div></div>'});
+                        el.popover('show');
+                        el.data('visible', true);
+                    }
+                );
+            }
         });
 
         $('.reporter_image_results table', appContext).on('click', '.iButton', function(e) {
